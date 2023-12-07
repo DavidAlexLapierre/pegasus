5743c072a3e4a6fd1fd7a92f210eb09bbed7bcf2
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-12-07 16:20:08 -0500

    initial window is now set up

--- Amethyst/src/core/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "core/test.h"
-
-#include <iostream>
-
-void Test::Init() {
-	std::cout << "Hello test" << std::endl;
-}
--- Amethyst/src/main.cpp
+++ Amethyst/src/main.cpp
@@ -1,6 +1,33 @@
-#include "core/test.h"
+#include <GLFW/glfw3.h>
 
 int main() {
-	auto test = Test();
-	test.Init();
+	GLFWwindow* window;
+
+	if (!glfwInit()) return -1;
+
+	window = glfwCreateWindow(640, 480, "Amethyst", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Update and draw code*/
+
+		glBegin(GL_TRIANGLES);
+
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
